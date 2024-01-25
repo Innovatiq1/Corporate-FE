d2b3809952227a3aac363e8adcb935a4169d7302
Ganesh M <ganesh.masilamani@innovatiqconsulting.com>
2024-01-25 15:55:11 +0530

    profile-settings

--- src/app/student/settings/settings.component.ts
+++ src/app/student/settings/settings.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { Student } from '@core/models/user.model';
 import { AuthenService } from '@core/service/authen.service';
 import { CertificateService } from '@core/service/certificate.service';
+import { EtmsService } from '@core/service/etms.service';
 
 import { StudentsService } from 'app/admin/students/all-students/students.service';
 import Swal from 'sweetalert2';
@@ -14,8 +20,9 @@ import Swal from 'sweetalert2';
   styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent {
-  stdForm: UntypedFormGroup ;
-  stdForm1: UntypedFormGroup ;
+  stdForm: UntypedFormGroup;
+  stdForm1: UntypedFormGroup;
+  profileForm: FormGroup;
   breadscrums = [
     {
       title: 'Settings',
@@ -26,7 +33,7 @@ export class SettingsComponent {
   editData: any;
   studentId: any;
   hide = true;
-  isEmailFieldDisabled=true;
+  isEmailFieldDisabled = true;
   files: any;
   fileName: any;
   avatar: any;
@@ -39,11 +46,17 @@ export class SettingsComponent {
   tcUrl: any;
   taUrl: any;
   adminUrl: any;
+  roname: any;
 
-  constructor(private studentService: StudentsService,private fb: UntypedFormBuilder, private certificateService:CertificateService,
-    private router: Router,private authenservice:AuthenService) {
-    
-    let urlPath = this.router.url.split('/')
+  constructor(
+    private studentService: StudentsService,
+    private etmsService: EtmsService,
+    private fb: UntypedFormBuilder,
+    private certificateService: CertificateService,
+    private router: Router,
+    private authenservice: AuthenService
+  ) {
+    let urlPath = this.router.url.split('/');
     this.cmUrl = urlPath.includes('coursemanager-settings');
     this.pmUrl = urlPath.includes('programmanager-settings');
     this.hodUrl = urlPath.includes('headofdepartment-settings');
@@ -51,183 +64,187 @@ export class SettingsComponent {
     this.tcUrl = urlPath.includes('trainingcoordinator-settings');
     this.taUrl = urlPath.includes('trainingadministrator-settings');
     this.adminUrl = urlPath.includes('admin-settings');
-    
-    
-    if(this.cmUrl===true){
+
+    if (this.cmUrl === true) {
       this.breadscrums = [
         {
-          title:'Settings',
+          title: 'Settings',
           items: ['Course Manager'],
           active: 'Settings',
         },
       ];
     }
-    if(this.pmUrl===true){
+    if (this.pmUrl === true) {
       this.breadscrums = [
         {
-          title:'Settings',
+          title: 'Settings',
           items: ['Program Manager'],
           active: 'Settings',
         },
       ];
     }
-    if(this.hodUrl===true){
+    if (this.hodUrl === true) {
       this.breadscrums = [
         {
-          title:'Settings',
+          title: 'Settings',
           items: ['Head Of Department'],
           active: 'Settings',
         },
       ];
     }
-    if(this.superUrl===true){
+    if (this.superUrl === true) {
       this.breadscrums = [
         {
-          title:'Settings',
+          title: 'Settings',
           items: ['Supervisor'],
           active: 'Settings',
         },
       ];
     }
-    if(this.tcUrl===true){
+    if (this.tcUrl === true) {
       this.breadscrums = [
         {
-          title:'Settings',
+          title: 'Settings',
           items: ['Training Coordinator'],
           active: 'Settings',
         },
       ];
     }
-    if(this.taUrl===true){
+    if (this.taUrl === true) {
       this.breadscrums = [
         {
-          title:'Settings',
+          title: 'Settings',
           items: ['Training Administrator'],
           active: 'Settings',
         },
       ];
     }
-    if(this.adminUrl===true){
+    if (this.adminUrl === true) {
       this.breadscrums = [
         {
-          title:'Settings',
+          title: 'Settings',
           items: ['Admin'],
           active: 'Settings',
         },
       ];
     }
     this.patchValues(),
-    //this.patchValues1()
-    this.stdForm = this.fb.group({
-      name: ['', [Validators.required, Validators.pattern('[a-zA-Z]+')]],
-      password:['', ],
-      currentpassword: ['', ],
-      
-    });
+      //this.patchValues1()
+      (this.stdForm = this.fb.group({
+        name: ['', [Validators.required, Validators.pattern('[a-zA-Z]+')]],
+        password: [''],
+        currentpassword: [''],
+      }));
     this.stdForm1 = this.fb.group({
       name: ['', [Validators.required, Validators.pattern('[a-zA-Z]+')]],
-      
+
       last_name: [''],
-      
+
       mobile: [''],
       city_name: ['', [Validators.required]],
       country_name: ['', [Validators.required]],
-      
+
       email: [
         '',
         [Validators.required, Validators.email, Validators.minLength(5)],
       ],
-     
+
       address: [''],
       avatar: [''],
-      
+    });
 
-      
+    this.profileForm = this.fb.group({
+      empName: ['', Validators.required],
+      designation: ['', Validators.required],
+      department: ['', Validators.required],
+      roName: ['', Validators.required],
+      director: ['', Validators.required],
+      TA: ['', Validators.required],
     });
 
     //constructor
   }
-  patchValues(){
-    this.studentId = localStorage.getItem('id')
-   // let studentId = localStorage.getItem('id')?localStorage.getItem('id'):null
+
+  ngOnInit() {
+    this.getUserProfile();
+  }
+  patchValues() {
+    this.studentId = localStorage.getItem('id');
+    // let studentId = localStorage.getItem('id')?localStorage.getItem('id'):null
     this.studentService.getStudentById(this.studentId).subscribe((res: any) => {
       this.editData = res;
       this.avatar = this.editData.avatar;
-      this.uploaded=this.avatar?.split('/')
+      this.uploaded = this.avatar?.split('/');
       this.uploadedImage = this.uploaded?.pop();
 
       this.stdForm.patchValue({
         name: this.editData.name,
         last_name: this.editData.last_name,
-        currentpassword:this.editData.password,
-        
-      })
+        currentpassword: this.editData.password,
+      });
       this.stdForm1.patchValue({
         name: this.editData.name,
         last: this.editData.last_name,
         rollNo: this.editData.rollNo,
         gender: this.editData.gender,
         mobile: this.editData.mobile,
-        
+
         email: this.editData.email,
         country_name: this.editData.country_name,
         city_name: this.editData.city_name,
-        
+
         address: this.editData.address,
-        uploadedImage:this.editData.avatar,
-        
-      })
-     })
+        uploadedImage: this.editData.avatar,
+      });
+    });
   }
-  onFileUpload(event:any) {
+  onFileUpload(event: any) {
     this.fileName = event.target.files[0].name;
-    this.files=event.target.files[0];
-    this.uploadedImage =event.target.files[0].name;
+    this.files = event.target.files[0];
+    this.uploadedImage = event.target.files[0].name;
 
     // const file = event.target.files[0];
     const formData = new FormData();
     // formData.append('files', file);
-    this.certificateService.uploadCourseThumbnail(formData).subscribe((response:any) => {
-    this.avatar = response.avatar;
-      this.uploaded=this.avatar.split('/')
-      this.uploadedImage = this.uploaded.pop();
-    });
+    this.certificateService
+      .uploadCourseThumbnail(formData)
+      .subscribe((response: any) => {
+        this.avatar = response.avatar;
+        this.uploaded = this.avatar.split('/');
+        this.uploadedImage = this.uploaded.pop();
+      });
   }
-
- 
 
   onSubmit() {
     console.log('Form Value', this.stdForm.value);
-    if(this.stdForm.valid){
+    if (this.stdForm.valid) {
       // this.instructor.uploadVideo(this.files).subscribe(
       //   (response: any) => {
       //     const inputUrl = response.inputUrl;
 
-          const userData: Student = this.stdForm.value;
-          //this.commonService.setVideoId(videoId)
+      const userData: Student = this.stdForm.value;
+      //this.commonService.setVideoId(videoId)
 
-          //userData.avatar = inputUrl;
-          //userData.filename= this.fileName
-          userData.type = "Student";
-          userData.role = "Student";
+      //userData.avatar = inputUrl;
+      //userData.filename= this.fileName
+      userData.type = 'Student';
+      userData.role = 'Student';
 
-          //this.currentVideoIds = [...this.currentVideoIds, ...videoId]
-          // this.currentVideoIds.push(videoId);
-          this.updateInstructor(userData);
+      //this.currentVideoIds = [...this.currentVideoIds, ...videoId]
+      // this.currentVideoIds.push(videoId);
+      this.updateInstructor(userData);
 
-          Swal.close();
-       // },
-
+      Swal.close();
+      // },
     }
   }
-  onSubmit1(){
-    console.log("========",this.stdForm1)
+  onSubmit1() {
+    console.log('========', this.stdForm1);
     if (!this.stdForm1.invalid) {
       this.studentService.uploadVideo(this.files).subscribe(
         (response: any) => {
           const inputUrl = response.inputUrl;
           this.authenservice.updateUserProfile(response.inputUrl);
-
 
           const userData: Student = this.stdForm1.value;
           //this.commonService.setVideoId(videoId)
@@ -253,32 +270,49 @@ export class SettingsComponent {
         }
       );
     }
-
-
   }
   private updateInstructor(userData: Student): void {
-    this.studentService.updateStudent(this.studentId,userData).subscribe(
+    this.studentService.updateStudent(this.studentId, userData).subscribe(
       () => {
         Swal.fire({
-          title: "Successful",
-          text: "Student data update successfully",
-          icon: "success",
+          title: 'Successful',
+          text: 'Student data update successfully',
+          icon: 'success',
         });
         //this.fileDropEl.nativeElement.value = "";
-      //this.stdForm.reset();
-      //this.toggleList()
-      //this.router.navigateByUrl('/admin/teachers/all-teachers');
+        //this.stdForm.reset();
+        //this.toggleList()
+        //this.router.navigateByUrl('/admin/teachers/all-teachers');
       },
-      (error: { message: any; error: any; }) => {
+      (error: { message: any; error: any }) => {
         Swal.fire(
-          "Failed to create course kit",
+          'Failed to create course kit',
           error.message || error.error,
-          "error"
+          'error'
         );
       }
     );
   }
+
+  /** Get User for profile */
+
+  getUserProfile() {
+    const userId = localStorage.getItem('id');
+    this.etmsService.getUserId(userId).subscribe((response: any) => {
+      console.log("from_Profile",response);
+        this.roname = response.roName
+        this.profileForm.patchValue({
+        empName: response.name,
+        designation: response.designation,
+        department: response.department,
+        roName: response.roName,
+        director: response.directorName,
+        TA: response.trainingAdminName,
+      })
+      console.log(this.profileForm.value);
+    });
+
+
+    
+  }
 }
-
-
-
