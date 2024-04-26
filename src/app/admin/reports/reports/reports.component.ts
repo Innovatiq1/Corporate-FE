0098b5458ae37879f066503f7c1465f5363eea57
Vaishnavi Kaithepalli <vaishnavi@innovatiqconsulting.com>
2024-04-26 16:49:23 +0530

    reports list integration

--- src/app/admin/reports/reports/reports.component.ts
+++ src/app/admin/reports/reports/reports.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CoursePaginationModel } from '@core/models/course.model';
+import { CourseService } from '@core/service/course.service';
+import { UtilsService } from '@core/service/utils.service';
 
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
   styleUrls: ['./reports.component.scss']
 })
-export class ReportsComponent {
+export class ReportsComponent implements OnInit{
   breadscrums = [
     {
       title: 'Reoprts',
@@ -15,23 +18,43 @@ export class ReportsComponent {
   ];
   displayedColumns: string[] = [
     // 'select',
-    'name',
+    'Report Generated',
     'Download',
     
   ];
-  dataSource = [
-    {reportName: 'Program report'},
-    {reportName: 'Course report'}]
-    
+  dataSource : any;
+  pageSizeArr = this.utils.pageSizeArr;
+  coursePaginationModel!: Partial<CoursePaginationModel>;
+  totalItems: any;
+
   constructor(
+    private courseService: CourseService,
+    private utils: UtilsService,
    ) {
-     
+    this.coursePaginationModel = {};
     
      }
 
   ngOnInit(): void {
-  
+  this.getReportsList();
   }
 
-
+  getReportsList(filters?: any) {
+    this.courseService.getAllSavedReports({...this.coursePaginationModel}).subscribe(
+      (response: any) => {
+        this.dataSource = response.data.docs;
+        this.totalItems = response.data.totalDocs;
+        this.coursePaginationModel.docs = response.data.docs;
+        this.coursePaginationModel.page = response.data.page;
+        this.coursePaginationModel.limit = response.data.limit;
+        this.coursePaginationModel.totalDocs = response.data.totalDocs;
+      },
+      
+    );
+  }
+  pageSizeChange($event: any) {
+    this.coursePaginationModel.page = $event?.pageIndex + 1;
+    this.coursePaginationModel.limit = $event?.pageSize;
+    this.getReportsList();
+  }
 }
