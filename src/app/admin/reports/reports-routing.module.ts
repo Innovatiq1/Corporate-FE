572b6888b750ec539bb97115351f024d1d986f77
Vaishnavi Kaithepalli <vaishnavi@innovatiqconsulting.com>
2024-04-26 14:54:16 +0530

    Generate report, report listing

--- src/app/admin/reports/reports-routing.module.ts
+++ src/app/admin/reports/reports-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ReportsComponent } from './reports/reports.component';
+import { GenereateReportComponent } from './genereate-report/genereate-report.component';
 
 const routes: Routes = [
   {
     path: 'report',
     component: ReportsComponent,
+  },
+  {
+    path: 'generate-report',
+    component: GenereateReportComponent,
   },
 
 
