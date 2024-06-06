09922056909952b256b5d544502286b630b511da
Ganesh M <ganesh.masilamani@innovatiqconsulting.com>
2024-06-06 19:26:05 +0530

    certificate-patch

--- src/app/student/settings/certificate-template/create-certificate/create-certificate.component.ts
+++ src/app/student/settings/certificate-template/create-certificate/create-certificate.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit,ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Renderer2,
+} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import Swal from 'sweetalert2';
@@ -9,7 +15,7 @@ import { CourseService } from '@core/service/course.service';
 @Component({
   selector: 'app-create-certificate',
   templateUrl: './create-certificate.component.html',
-  styleUrls: ['./create-certificate.component.scss']
+  styleUrls: ['./create-certificate.component.scss'],
 })
 export class CreateCertificateComponent implements OnInit {
   breadscrums = [
@@ -23,24 +29,25 @@ export class CreateCertificateComponent implements OnInit {
   certificateForm!: FormGroup;
   isSubmitted = false;
   editUrl!: boolean;
-  
+
   classId!: any;
   viewUrl: boolean;
 
   title: boolean = false;
-  submitted : boolean = false;
+  submitted: boolean = false;
   course: any;
   thumbnail: any;
   image_link: any;
   uploaded: any;
   uploadedImage: any;
-  constructor(private fb:FormBuilder,
+  constructor(
+    private fb: FormBuilder,
     private router: Router,
     private _activeRoute: ActivatedRoute,
     private certificateService: CertificateService,
-    private courseService:CourseService, private renderer: Renderer2
-
-  ){
+    private courseService: CourseService,
+    private renderer: Renderer2
+  ) {
     this._activeRoute.queryParams.subscribe((params) => {
       this.classId = params['id'];
       if (this.classId) {
@@ -51,18 +58,16 @@ export class CreateCertificateComponent implements OnInit {
     this.editUrl = urlPath.includes('edit');
     this.viewUrl = urlPath.includes('view');
 
-if(this.editUrl==true)
-  {
-    this.breadscrums = [
-      {
-        title: 'Certificate',
-        items: ['Certificate'],
-        active: 'Edit Certificate',
-      },
-    ];
-  }
-  if(this.viewUrl==true)
-    {
+    if (this.editUrl == true) {
+      this.breadscrums = [
+        {
+          title: 'Certificate',
+          items: ['Certificate'],
+          active: 'Edit Certificate',
+        },
+      ];
+    }
+    if (this.viewUrl == true) {
       this.breadscrums = [
         {
           title: 'Certificate',
@@ -71,23 +76,20 @@ if(this.editUrl==true)
         },
       ];
     }
-   if(this.editUrl || this.viewUrl){
-    this.getData();
-   } 
-
-  
-
-   
+    if (this.editUrl || this.viewUrl) {
+      this.getData();
+    }
   }
 
-  edit(){
-    this.router.navigate(['/student/settings/certificate/edit/:id'], {queryParams:{id:this.classId}})
-    }
-
+  edit() {
+    this.router.navigate(['/student/settings/certificate/edit/:id'], {
+      queryParams: { id: this.classId },
+    });
+  }
 
   // let urlPath = this.router.url.split('/');
   // this.editUrl = urlPath.includes('edit-class');
-  ngOnInit(){
+  ngOnInit() {
     this.certificateForm = this.fb.group({
       title: [''],
       user: [''],
@@ -101,8 +103,8 @@ if(this.editUrl==true)
       text6: [''],
       text7: [''],
       text8: [''],
-     
-    });  }
+    });
+  }
   // ngOnInit(){
   //   this.certificateForm = this.fb.group({
   //     title: ['', Validators.required],
@@ -117,9 +119,9 @@ if(this.editUrl==true)
   //     text6: ['', Validators.required],
   //     text7: ['', Validators.required],
   //     text8: ['', Validators.required],
-     
+
   //   });  }
-   
+
   onFileUpload(event: any) {
     const file = event.target.files[0];
 
@@ -143,144 +145,179 @@ if(this.editUrl==true)
       });
     }
   }
-  
-    // onFileUpload(event:any) {
-    //   const file = event.target.files[0];
-    
-    //   this.thumbnail = file
-    //   const formData = new FormData();
-    //   formData.append('files', this.thumbnail);
-    // this.courseService.uploadCourseThumbnail(formData).subscribe((data: any) =>{
-    //   this.image_link = data.data.thumbnail.replace(/\//g, '\\');
-    //   this.setBackgroundImage(this.image_link);
-    //   this.uploaded=this.image_link?.split('/')
-    //   let image  = this.uploaded?.pop();
-    //   this.uploaded= image?.split('\\');
-    //   this.uploadedImage = this.uploaded?.pop();
-    
-    // })
-    //   // this.certificateService.uploadCourseThumbnail(formData).subscribe((response:any) => {
-    //   //   this.image_link = response.image_link;
-    //   //   console.log("imagesss",this.image_link)
-    //   //   this.uploaded=this.image_link.split('/')
-    //   //   this.uploadedImage = this.uploaded.pop();
-    //   //   console.log("uploaded",this.uploadedImage)
-    //   //   this.firstFormGroup.patchValue({
-    //   //     // image_link: response,
-    //   //   });
-    //   // });
-    // }
-    
-    private setBackgroundImage(imageUrl: string) {
-  
-      this.backgroundTable.nativeElement.style.backgroundImage = `url("${imageUrl}")`;
-      setTimeout(() => {
-        const computedStyle = window.getComputedStyle(this.backgroundTable.nativeElement);
-        console.log('Computed background image:', computedStyle.backgroundImage);
-      }, 1000);
-    }
-  
 
+  // onFileUpload(event:any) {
+  //   const file = event.target.files[0];
 
+  //   this.thumbnail = file
+  //   const formData = new FormData();
+  //   formData.append('files', this.thumbnail);
+  // this.courseService.uploadCourseThumbnail(formData).subscribe((data: any) =>{
+  //   this.image_link = data.data.thumbnail.replace(/\//g, '\\');
+  //   this.setBackgroundImage(this.image_link);
+  //   this.uploaded=this.image_link?.split('/')
+  //   let image  = this.uploaded?.pop();
+  //   this.uploaded= image?.split('\\');
+  //   this.uploadedImage = this.uploaded?.pop();
 
-    saveCertificate() {
-      console.log(this.certificateForm)
-      if(this.certificateForm.valid) {
+  // })
+  //   // this.certificateService.uploadCourseThumbnail(formData).subscribe((response:any) => {
+  //   //   this.image_link = response.image_link;
+  //   //   console.log("imagesss",this.image_link)
+  //   //   this.uploaded=this.image_link.split('/')
+  //   //   this.uploadedImage = this.uploaded.pop();
+  //   //   console.log("uploaded",this.uploadedImage)
+  //   //   this.firstFormGroup.patchValue({
+  //   //     // image_link: response,
+  //   //   });
+  //   // });
+  // }
+
+  private setBackgroundImage(imageUrl: string) {
+    console.log('setBackgroundImage');
+    this.backgroundTable.nativeElement.style.backgroundImage = `url("${imageUrl}")`;
+    setTimeout(() => {
+      const computedStyle = window.getComputedStyle(
+        this.backgroundTable.nativeElement
+      );
+      console.log('Computed background image:', computedStyle.backgroundImage);
+    }, 1000);
+  }
+
+  saveCertificate() {
+    console.log(this.certificateForm);
+    if (this.certificateForm.valid) {
       if (!this.editUrl) {
-        
         //let sessions = this.getSession();
-       // if (sessions) {
+        // if (sessions) {
         //  this.certificateForm.value.sessions = sessions;
         //  this.certificateForm.value.programName = this.courseTitle;
-          this.isSubmitted = true;
-  
-          Swal.fire({
-            title: 'Are you sure?',
-            text: 'Do you want to create Certificate!',
-            icon: 'warning',
-            confirmButtonText: 'Yes',
-            showCancelButton: true,
-            cancelButtonColor: '#d33',
-          }).then((result) => {
-            if (result.isConfirmed) {
-            this.certificateForm.value.image=this.image_link
-              this.certificateService
-                .createCertificate(this.certificateForm.value)
-                .subscribe((response: any) => {
-                  Swal.fire({
-                    title: 'Success',
-                    text: 'Certificate Created successfully.',
-                    icon: 'success',
-                    // confirmButtonColor: '#d33',
-                  });
-                  this.router.navigateByUrl(`/student/settings/certificate/template`);
+        this.isSubmitted = true;
+
+        Swal.fire({
+          title: 'Are you sure?',
+          text: 'Do you want to create Certificate!',
+          icon: 'warning',
+          confirmButtonText: 'Yes',
+          showCancelButton: true,
+          cancelButtonColor: '#d33',
+        }).then((result) => {
+          if (result.isConfirmed) {
+            this.certificateForm.value.image = this.image_link;
+            this.certificateService
+              .createCertificate(this.certificateForm.value)
+              .subscribe((response: any) => {
+                Swal.fire({
+                  title: 'Success',
+                  text: 'Certificate Created successfully.',
+                  icon: 'success',
+                  // confirmButtonColor: '#d33',
                 });
-            }
-          });
-      //  }
+                this.router.navigateByUrl(
+                  `/student/settings/certificate/template`
+                );
+              });
+          }
+        });
+        //  }
       }
       if (this.editUrl) {
-       // let sessions = this.getSession();
+        // let sessions = this.getSession();
         // if (sessions) {
         //   this.classForm.value.sessions = sessions;
         //   this.classForm.value.programName = this.courseTitle;
-          Swal.fire({
-            title: 'Are you sure?',
-            text: 'You want to update this class!',
-            icon: 'warning',
-            confirmButtonText: 'Yes',
-            showCancelButton: true,
-            cancelButtonColor: '#d33',
-          }).then((result) => {
-            if (result.isConfirmed) {
-              this.certificateService
-                .updateCertificate(this.classId, this.certificateForm.value)
-                .subscribe((response: any) => {
-                  Swal.fire({
-                    title: 'Success',
-                    text: 'Certificate updated successfully.',
-                    icon: 'success',
-                    // confirmButtonColor: '#d33',
-                  });
-                  window.history.back();
+        Swal.fire({
+          title: 'Are you sure?',
+          text: 'You want to update this class!',
+          icon: 'warning',
+          confirmButtonText: 'Yes',
+          showCancelButton: true,
+          cancelButtonColor: '#d33',
+        }).then((result) => {
+          if (result.isConfirmed) {
+            this.certificateForm.value.image = this.image_link;
+            this.certificateService
+              .updateCertificate(this.classId, this.certificateForm.value)
+              .subscribe((response: any) => {
+                Swal.fire({
+                  title: 'Success',
+                  text: 'Certificate updated successfully.',
+                  icon: 'success',
+                  // confirmButtonColor: '#d33',
                 });
-            }
-          });
-       // }
+                window.history.back();
+              });
+          }
+        });
+        // }
       }
-    }else{
-    //  this.classForm.markAllAsTouched();
+    } else {
+      //  this.classForm.markAllAsTouched();
       this.submitted = true;
     }
-    }
-    getData() {
-      forkJoin({
-       
-        course: this.certificateService.getCertificateById(this.classId),
-      }).subscribe((response: any) => {
-     
-        this.course = response.course;
-       
-     
-        this.certificateForm.patchValue({
-          title: this.course?.title,
-          user:this.course?.user,
-          course:this.course?.course,
-          completionDate:this.course?.completionDate,
-          text1:this.course?.text1,
-          text2:this.course?.text2,
-          text3:this.course?.text3,
-          text4:this.course?.text4,
-          text5:this.course?.text5,
-          text6:this.course?.text6,
-          text7:this.course?.text7,
-          text8:this.course?.text8,
-        });
-      });
- 
- 
   }
-   
+  // getData() {
+  //   forkJoin({
+  //     course: this.certificateService.getCertificateById(this.classId),
+  //   }).subscribe((response: any) => {
+  //     this.course = response.course;
 
+  //     let imageUrl = this.course?.image;
+  //     imageUrl = imageUrl.replace(/\\/g, '/');
+  //     imageUrl = encodeURI(imageUrl);
+  //     this.setBackgroundImage(imageUrl);
+  //     this.uploaded = imageUrl?.split('/');
+  //     let image = this.uploaded?.pop();
+  //     this.uploaded = image?.split('\\');
+  //     this.uploadedImage = this.uploaded?.pop();
 
+  //     this.certificateForm.patchValue({
+  //       title: this.course?.title,
+  //       user: this.course?.user,
+  //       course: this.course?.course,
+  //       completionDate: this.course?.completionDate,
+  //       text1: this.course?.text1,
+  //       text2: this.course?.text2,
+  //       text3: this.course?.text3,
+  //       text4: this.course?.text4,
+  //       text5: this.course?.text5,
+  //       text6: this.course?.text6,
+  //       text7: this.course?.text7,
+  //       text8: this.course?.text8,
+  //     });
+  //   });
+  // }
+
+  getData() {
+    forkJoin({
+      course: this.certificateService.getCertificateById(this.classId)
+    }).subscribe((response: any) => {
+      this.course = response.course;
+
+      let imageUrl = this.course?.image;
+      imageUrl = imageUrl.replace(/\\/g, '/');
+      imageUrl = encodeURI(imageUrl);
+      this.setBackgroundImage(imageUrl);
+      
+      this.uploaded = imageUrl?.split('/');
+      let image = this.uploaded?.pop();
+      this.uploaded = image?.split('\\');
+      this.uploadedImage = this.uploaded?.pop();
+
+      this.certificateForm.patchValue({
+        title: this.course?.title,
+        user: this.course?.user,
+        course: this.course?.course,
+        completionDate: this.course?.completionDate,
+        text1: this.course?.text1,
+        text2: this.course?.text2,
+        text3: this.course?.text3,
+        text4: this.course?.text4,
+        text5: this.course?.text5,
+        text6: this.course?.text6,
+        text7: this.course?.text7,
+        text8: this.course?.text8
+      });
+    });
+  }
 }
